6c7714ec5667c5dbbb8a0c3e78e7cd3f19d625b4
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-26 23:07:58 -0400

    Destructure array of objects
    incomplete trouble displaying data to screen via destructing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+// //CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+let [honda, tesla] = cars;
+
+for (const {
+  model: name,
+  coloursByPopularity: color,
+  speedStats: { topSpeed: mph }
+} of cars) {
+  console.log(
+    "model: " + name + " Popular color: " + color[0] + ", speedStats: " + mph
+  );
+}
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{tesla.TopSpeed}</td>
+      <td>{tesla.TopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{honda.TopSpeed}</td>
+      <td>{honda.TopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
