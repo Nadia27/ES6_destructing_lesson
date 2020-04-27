ccb95e5b18f250759d016aa942acca82278b195d
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-27 19:53:23 -0400

    Destructing - lesson/practice
    Complete lesson  with practice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+//import animals from "./data";
+
+//Destructing
+
+//console.log(animals);
+//const [cat, dog] = animals; //Destructure animals array
+
+//cat and dog are pulled out of array and are now objects that can be destructured
+
+//console.log(cat);
+
+//Destructing objects
+//to change the property name
+//const {name: dogName, sound: dogSound} = dog;
+
+// console.log(dogName);
+// console.log(dogSound);
+
+// Provide default values
+// const { name = "Bones", sound = "Bark" } = dog;
+
+//Grab nested values
+//const {
+//feedingRequirements: { food, water }
+//} = dog;
+
+//console.log(food);
+
+// To change the variable names
+
 // //CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
@@ -5,31 +35,38 @@ import cars from "./practice";
 
 let [honda, tesla] = cars;
 
-for (const {
-  model: name,
-  coloursByPopularity: color,
-  speedStats: { topSpeed: mph }
-} of cars) {
-  console.log(
-    "model: " + name + " Popular color: " + color[0] + ", speedStats: " + mph
-  );
-}
+console.log(tesla);
+
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
 
 ReactDOM.render(
   <table>
     <tr>
       <th>Brand</th>
       <th>Top Speed</th>
+      <th>Top Color</th>
     </tr>
     <tr>
       <td>{tesla.model}</td>
-      <td>{tesla.TopSpeed}</td>
-      <td>{tesla.TopColour}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
     </tr>
     <tr>
       <td>{honda.model}</td>
-      <td>{honda.TopSpeed}</td>
-      <td>{honda.TopColour}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
     </tr>
   </table>,
   document.getElementById("root")
